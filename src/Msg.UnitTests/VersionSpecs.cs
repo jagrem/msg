838a7d90e716d2352253cb1c638fdde2ba466a19
jagrem <jagremc@gmail.com>
2014-10-12 22:02:55 +1300

    Add specs for implicit byte array to version conversion where byte array is invalid.

--- src/Msg.UnitTests/VersionSpecs.cs
+++ src/Msg.UnitTests/VersionSpecs.cs
@@ -1,12 +1,16 @@
 ﻿using NUnit.Framework;
 using Msg.Domain;
 using FluentAssertions;
+using System;
+using Version = Msg.Domain.Version;
 
 namespace Msg.UnitTests
 {
 	[TestFixture]
 	public class VersionSpecs
 	{
+		static readonly Func<byte[],Version> convertByteArray = b => b;
+
 		[Test]
 		public void Given_any_version_When_converting_to_a_byte_array_Then_AMQP_followed_by_zero_are_the_first_five_bytes()
 		{
@@ -47,6 +51,46 @@ namespace Msg.UnitTests
 			result.Major.Should ().Be (1);
 			result.Minor.Should ().Be (2);
 			result.Revision.Should ().Be (3);
+		}
+
+		[Test]
+		public void Given_an_empty_byte_array_When_converting_to_a_version_Then_an_exception_is_thrown()
+		{
+			var subject = new byte[0];
+			Action result = () => convertByteArray (subject);
+			result.ShouldThrow<ArgumentException> ();
+		}
+
+		[Test]
+		public void Given_a_byte_array_that_is_too_short_When_converting_to_a_version_Then_an_exception_is_thrown()
+		{
+			var subject = new byte[] { (byte)'Z', (byte)'S', (byte)'X', (byte)'F', (byte)0, (byte)1, (byte)2 };
+			Action result = () => convertByteArray (subject);
+			result.ShouldThrow<ArgumentException> ();
+		}
+
+		[Test]
+		public void Given_a_byte_array_that_is_too_long_When_converting_to_a_version_Then_an_exception_is_thrown()
+		{
+			var subject = new byte[] { (byte)'Z', (byte)'S', (byte)'X', (byte)'F', (byte)0, (byte)1, (byte)2, (byte)3, (byte)4 };
+			Action result = () => convertByteArray (subject);
+			result.ShouldThrow<ArgumentException> ();
+		}
+
+		[Test]
+		public void Given_a_byte_array_with_an_fifth_byte_greater_than_zero_When_converting_to_a_version_Then_an_exception_is_thrown()
+		{
+			var subject = new byte[] { (byte)'Z', (byte)'S', (byte)'X', (byte)'F', (byte)7, (byte)1, (byte)2, (byte)3 };
+			Action result = () => convertByteArray (subject);
+			result.ShouldThrow<ArgumentException> ();
+		}
+
+		[Test]
+		public void Given_a_byte_array_that_doesnt_start_with_AMQP_When_converting_to_a_version_Then_an_exception_is_thrown()
+		{
+			var subject = new byte[] { (byte)'Z', (byte)'S', (byte)'X', (byte)'F', (byte)7, (byte)1, (byte)2, (byte)3 };
+			Action result = () => convertByteArray (subject);
+			result.ShouldThrow<ArgumentException> ();
 		}
 	}
 }
