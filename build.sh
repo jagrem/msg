6c74f254834f25d22012e812300766c5031f3801
jagrem <jagremc@gmail.com>
2014-10-21 20:33:58 +1300

    Update Travis-CI configuration with Mac version.

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+sudo add-apt-repository ppa:directhex/monoxide
+sudo apt-get install mono-devel mono-gmcs
 cd ./src
-EnableNugetPackageRestore=true xbuild Msg.sln /p:Configuration=Release
+xbuild Msg.sln /p:Configuration=Release
