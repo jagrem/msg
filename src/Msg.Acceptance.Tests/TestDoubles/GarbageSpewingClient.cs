f2f4f07e3d1ef9fb5ded847438a133bb79d9583a
jagrem <jagremc@gmail.com>
2014-10-31 23:37:59 +1300

    Move garbage spewing client to test doubles.

---
+++ src/Msg.Acceptance.Tests/TestDoubles/GarbageSpewingClient.cs
@@ -0,0 +1,29 @@
+using System.Threading.Tasks;
+using Version = Msg.Domain.Version;
+using System;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Msg.Acceptance.Tests
+{
+
+	class GarbageSpewingClient
+	{
+		TcpClient client;
+
+		public async Task<byte[]> ConnectAsync() {
+			client = new TcpClient ();
+			await client.ConnectAsync (IPAddress.Loopback, 1984);
+			var stream = client.GetStream ();
+			await stream.WriteAsync (new byte[] { 1, 9, 8, 4 }, 0, 4);
+			var buffer = new byte[8];
+			await stream.ReadAsync (buffer, 0, 8);
+			return buffer;
+		}
+
+		public bool IsConnected()
+		{
+			return client != null && client.Connected;
+		}
+	}
+}
