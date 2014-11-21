3b842b1f39f73f4362ccc4977c04ad829a678a4d
jagrem <jagremc@gmail.com>
2014-11-22 05:44:58 +1300

    Convert tabs to spaces for specs project.

--- Msg.Core.Specs/Transport/Connections/ConnectionFactoryExtensions.cs
+++ Msg.Core.Specs/Transport/Connections/ConnectionFactoryExtensions.cs
@@ -3,12 +3,12 @@ using Msg.Core.Specs.Transport.Connections.Replay;
 
 namespace Msg.Core.Specs.Transport.Connections
 {
-	public static class ConnectionFactoryExtensions
-	{
-		public static ReplayConnection CreateReplayConnectionAsync (this ConnectionFactory factory)
-		{
-			return new ReplayConnection ();
-		}
-	}
+    public static class ConnectionFactoryExtensions
+    {
+        public static ReplayConnection CreateReplayConnectionAsync (this ConnectionFactory factory)
+        {
+            return new ReplayConnection ();
+        }
+    }
 }
 
--- Msg.Core.Specs/Transport/Frames/FrameBodySpecs.cs
+++ Msg.Core.Specs/Transport/Frames/FrameBodySpecs.cs
@@ -9,43 +9,43 @@ using Msg.Core.Transport.Frames.Constants;
 
 namespace Msg.Core.Specs.Transport.Frames
 {
-	[TestFixture]
-	public class FrameBodySpecs
-	{
-		[Test]
-		public void Given_a_frame_body_byte_array_with_no_performative_When_creating_a_frame_body_Then_throw_exception ()
-		{
-			var frameBodyBytes = new byte[] { 0, 0, 0, 0, 0 };
-			Func<Task> action = async () => await FrameBodyFactory.GetFrameBodyFromBytes (frameBodyBytes);
-			action.ShouldThrow<MalformedFrameException> ()
+    [TestFixture]
+    public class FrameBodySpecs
+    {
+        [Test]
+        public void Given_a_frame_body_byte_array_with_no_performative_When_creating_a_frame_body_Then_throw_exception ()
+        {
+            var frameBodyBytes = new byte[] { 0, 0, 0, 0, 0 };
+            Func<Task> action = async () => await FrameBodyFactory.GetFrameBodyFromBytes (frameBodyBytes);
+            action.ShouldThrow<MalformedFrameException> ()
 				.WithMessage ("Unrecognised frame type.");
-		}
+        }
 
-		[Test]
-		[TestCase ("Attach", PerformativeType.Attach)]
-		[TestCase ("Begin", PerformativeType.Begin)]
-		[TestCase ("Open", PerformativeType.Open)]
-		[TestCase ("Flow", PerformativeType.Flow)]
-		[TestCase ("Transfer", PerformativeType.Transfer)]
-		[TestCase ("Disposition", PerformativeType.Disposition)]
-		[TestCase ("Close", PerformativeType.Close)]
-		[TestCase ("Detach", PerformativeType.Detach)]
-		[TestCase ("End", PerformativeType.End)]
-		public async Task Given_a_frame_body_byte_array_with_a_performative_When_creating_a_frame_body_Then_performative_equals_byte_array_performative (string performative, PerformativeType expectedType)
-		{
-			var frameBodyBytes = ConvertToByteArray (performative);
-			var result = await FrameBodyFactory.GetFrameBodyFromBytes (frameBodyBytes);
-			result.Performative.Should ().Be (expectedType);
-		}
+        [Test]
+        [TestCase ("Attach", PerformativeType.Attach)]
+        [TestCase ("Begin", PerformativeType.Begin)]
+        [TestCase ("Open", PerformativeType.Open)]
+        [TestCase ("Flow", PerformativeType.Flow)]
+        [TestCase ("Transfer", PerformativeType.Transfer)]
+        [TestCase ("Disposition", PerformativeType.Disposition)]
+        [TestCase ("Close", PerformativeType.Close)]
+        [TestCase ("Detach", PerformativeType.Detach)]
+        [TestCase ("End", PerformativeType.End)]
+        public async Task Given_a_frame_body_byte_array_with_a_performative_When_creating_a_frame_body_Then_performative_equals_byte_array_performative (string performative, PerformativeType expectedType)
+        {
+            var frameBodyBytes = ConvertToByteArray (performative);
+            var result = await FrameBodyFactory.GetFrameBodyFromBytes (frameBodyBytes);
+            result.Performative.Should ().Be (expectedType);
+        }
 
-		static byte[] ConvertToByteArray (string expectedPerformative)
-		{
-			var performativeBytes = Encoding.UTF8.GetBytes (expectedPerformative);
-			var frameBodyBytes = new byte[performativeBytes.Length + 1];
-			Array.Copy (performativeBytes, frameBodyBytes, performativeBytes.Length);
-			frameBodyBytes [performativeBytes.Length] = 0;
-			return frameBodyBytes;
-		}
-	}
+        static byte[] ConvertToByteArray (string expectedPerformative)
+        {
+            var performativeBytes = Encoding.UTF8.GetBytes (expectedPerformative);
+            var frameBodyBytes = new byte[performativeBytes.Length + 1];
+            Array.Copy (performativeBytes, frameBodyBytes, performativeBytes.Length);
+            frameBodyBytes [performativeBytes.Length] = 0;
+            return frameBodyBytes;
+        }
+    }
 }
 
