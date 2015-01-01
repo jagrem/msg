9eefde2e064e7f2d3e130cfe0438305e31b83477
jagrem <jagremc@gmail.com>
2015-01-01 19:17:41 +1300

    Extract methods for creating listeners and notifying consumers.
    Extract method for creating a TCP listener.
    
    Extract method for notifying clients when the listener has been created using a task completion source.

--- Msg.Core.Specs/Transport/Connections/Tcp/TcpServer.cs
+++ Msg.Core.Specs/Transport/Connections/Tcp/TcpServer.cs
@@ -28,11 +28,23 @@ namespace Msg.Core.Specs.Transport.Connections.Tcp
         static void Start (TaskCompletionSource<int> completionSource, TaskCompletionSource<byte[]> result, CancellationToken cancellationToken)
         {
             Task.Factory.StartNew (async () => {
-                var server = new TcpListener (IPAddress.Loopback, 9876);
-                server.Start ();
-                completionSource.SetResult (0);
-                await ListenForClients (cancellationToken, server, result);
+                var endpoint = new TcpEndpoint (IPAddress.Loopback, (PortNumber)9876);
+                var listener = CreateListener (endpoint);
+                NotifyListenerCreated (completionSource);
+                await ListenForClients (cancellationToken, listener, result);
             });
+        }
+
+        static TcpListener CreateListener (TcpEndpoint endpoint)
+        {
+            var listener = new TcpListener (endpoint.IpAddress, endpoint.Port);
+            listener.Start ();
+            return listener;
+        }
+
+        static void NotifyListenerCreated (TaskCompletionSource<int> completionSource)
+        {
+            completionSource.SetResult (0);
         }
 
         static async Task ListenForClients (CancellationToken cancellationToken, TcpListener server, TaskCompletionSource<byte[]> result)
