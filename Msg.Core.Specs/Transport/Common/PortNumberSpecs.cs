b7ee43a35e65ae095d5eb7d1ae754840f4b31055
jagrem <jagremc@gmail.com>
2015-01-01 19:15:37 +1300

    Add specs for port number type implicit and explicit conversions.

---
+++ Msg.Core.Specs/Transport/Common/PortNumberSpecs.cs
@@ -0,0 +1,62 @@
+﻿using NUnit.Framework;
+using Msg.Core.Transport.Common;
+using System;
+using FluentAssertions;
+
+namespace Msg.Core.Specs.Transport.Common
+{
+    [TestFixture]
+    public class PortNumberSpecs
+    {
+        static PortNumber CastTo (int value)
+        {
+            return (PortNumber)value;
+        }
+
+        [Test]
+        public void Given_a_value_greater_than_the_maximum_port_number_allowed_When_casting_to_a_port_number_Then_it_should_throw (int value)
+        {
+            //-----------------------------------------------------------------------------------------------------------
+            // Act
+            //-----------------------------------------------------------------------------------------------------------
+            Action result = () => CastTo (value);
+
+            //-----------------------------------------------------------------------------------------------------------
+            // Assert
+            //-----------------------------------------------------------------------------------------------------------
+            result.ShouldThrow<ArgumentOutOfRangeException> ();
+        }
+
+        [Test]
+        [TestCase (0)]
+        [TestCase (-1)]
+        public void Given_a_value_less_than_or_equal_to_zero_When_casting_to_a_port_number_Then_it_should_throw (int value)
+        {
+            //-----------------------------------------------------------------------------------------------------------
+            // Act
+            //-----------------------------------------------------------------------------------------------------------
+            Action result = () => CastTo (value);
+
+            //-----------------------------------------------------------------------------------------------------------
+            // Assert
+            //-----------------------------------------------------------------------------------------------------------
+            result.ShouldThrow<ArgumentOutOfRangeException> ();
+        }
+
+        [Test]
+        public void Given_a_value_When_casting_to_a_port_number_Then_the_port_number_equals_the_value ()
+        {
+            //-----------------------------------------------------------------------------------------------------------
+            // Act
+            //-----------------------------------------------------------------------------------------------------------
+            var result = CastTo (1028);
+
+            //-----------------------------------------------------------------------------------------------------------
+            // Assert
+            //-----------------------------------------------------------------------------------------------------------
+            result.Should ().BeOfType<PortNumber> ();
+            ((int)result).Should ().Be (1028);
+        }
+    }
+}
+
