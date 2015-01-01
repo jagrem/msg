ab53a373d6a8afdce1578d7dd899e20c54d50b2e
jagrem <jagremc@gmail.com>
2015-01-02 00:06:04 +1300

    Make async version negotiation method obsolete.

--- Msg.Core/Versioning/VersionNegotiator.cs
+++ Msg.Core/Versioning/VersionNegotiator.cs
@@ -7,6 +7,7 @@ namespace Msg.Core.Versioning
 {
     public static class VersionNegotiator
     {
+        [System.Obsolete("This method is deprecated us VersionNegotiator.Select instead.")]
         public static async Task<Version> NegotiateVersionAsync (IEnumerable<VersionRange> supportedVersions)
         {
             return await Task.FromResult (supportedVersions.First ().UpperBoundInclusive);
