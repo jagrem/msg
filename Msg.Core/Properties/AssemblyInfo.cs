3003fae0aa47c2301c2e43712865006d2c57b765
jagrem <jagremc@gmail.com>
2015-01-02 18:09:49 +1300

    Strong name assemblies for identity.

--- Msg.Core/Properties/AssemblyInfo.cs
+++ Msg.Core/Properties/AssemblyInfo.cs
@@ -20,4 +20,4 @@ using System.Runtime.CompilerServices;
 //[assembly: AssemblyDelaySign(false)]
 //[assembly: AssemblyKeyFile("")]
 
-[assembly: InternalsVisibleToAttribute("Msg.Core.Specs")]
+[assembly: InternalsVisibleToAttribute("Msg.Core.Specs,PublicKey=0024000004800000940000000602000000240000525341310004000011000000137c01da3794da1954a603bc0c2c84a1be82b0e082184b9567f11bd5c0f29ff969bdcb7108164e4aaa51e63b29e321f04a8d719edc2ae30971ddd6c6103b202631135b2fb2414ba45ea7b97a854eec83cfaf9e7b01e9552d42bbf981c319e198e2df450a6dc615249a518cbc92d544a2073c63447e5e475bf2e8ae55ee1a83e0")]
