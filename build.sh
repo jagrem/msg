631688b0bdfc7a669914b1c8c22d9cefe45789df
jagrem <jagremc@gmail.com>
2015-01-03 21:22:20 +1300

    Refactor build script and fix build error.

--- build.sh
+++ build.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
+NUGET=.nuget/NuGet.exe
+NUNIT_VERSION=2.6.4
 
 # NuGet package restore
-mono .nuget/NuGet.exe restore
+mono $NUGET restore
 
 # Build solution
 xbuild /p:Configuration=Release
 
-# Test
-NUNIT_VERSION=2.6.4
-
 # Install NUnit test runner
-mono .nuget/NuGet.exe install NUnit.Runners -Version $NUNIT_VERSION -OutputDirectory packages
+mono $NUGET install NUnit.Runners -Version $NUNIT_VERSION -OutputDirectory packages
 
 # Run tests
-mono --runtime=v4.5 packages/NUnit.Runners.$NUNIT_VERSION/tools/nunit-console.exe */bin/Release/*.Specs.dll -nologo -noxml -nodots -labels
+mono packages/NUnit.Runners.$NUNIT_VERSION/tools/nunit-console.exe */*/bin/Release/*.Specs.dll -nologo -noxml -nodots -labels
