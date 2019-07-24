707045bba65fe11dd031a3d672e863601a12973e
jagrem <jagremc@gmail.com>
2019-07-24 21:35:34 +1200

    Make base class abstract
    Make base AMQP frame class abstract and remove byte array content.

--- Core/Msg.Core/Transport/Frames/Amqp/AmqpFrame.cs
+++ Core/Msg.Core/Transport/Frames/Amqp/AmqpFrame.cs
@@ -2,13 +2,12 @@ using Msg.Core.Transport.Frames.Constants;
 
 namespace Msg.Core.Transport.Frames.Amqp
 {
-    public class AmqpFrame
+    public abstract class AmqpFrame
     {
         public ChannelId ChannelId { get; }
         public PerformativeType Peformative { get; }
-        public byte[] Payload { get; }
 
-        public AmqpFrame(ChannelId channelId, PerformativeType performative)
+        protected AmqpFrame(ChannelId channelId, PerformativeType performative)
         {
             ChannelId = channelId;
             Peformative = performative;
